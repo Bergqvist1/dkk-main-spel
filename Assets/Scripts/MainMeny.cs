47857aa0c40a261e42d00fdc02392deb6035602c
Bergqvist1 <sibe05001@utb.vaxjo.se>
2023-11-22 09:38:23 +0100

    Initial commit

---
+++ Assets/Scripts/MainMeny.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMeny : MonoBehaviour
+{
+    public void Spela()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Spelaren har slutat spela");
+    }
+}
